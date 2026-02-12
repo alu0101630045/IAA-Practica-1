ddfb656a08da44ca5a3935364f0519673c2294bc
alu0101630045 <alu0101630045@ull.edu.es>
2026-02-12 11:36:56 +0000

    Comentarios añadidos

--- src/Functions.cc
+++ src/Functions.cc
@@ -12,34 +12,35 @@
 std::vector<double> prob_cond_bin(const std::vector<double>& p, int N, int maskC, int valC, int maskI) {
   int n_interes = 0;
   int temp = maskI;
-  while (temp > 0) {
-    if (temp & 1) n_interes++;
-    temp >>= 1;
+  while (temp > 0) { // Contar todos los 1 en maskI
+    if (temp & 1) n_interes++; 
+    temp >>= 1; 
   }
-  std::vector<double> result(1 << n_interes, 0.0);
+  std::vector<double> result(1 << n_interes, 0.0); // Resultado con tamaño 2^n_interes
 
   double prob_evidencia = 0.0;
   int size_p = p.size();
 
-  for (int k = 0; k < size_p; ++k) {
-    if ((k & maskC) == valC) {
+  // Iteramos sobre todas las combinaciones posibles
+  for (int k = 0; k < size_p; ++k) { 
+    if ((k & maskC) == valC) { // Aplicamos la máscara a k para quitar las variables que no importan
       int idx_res = 0;
       int current_pos = 0;
 
       for (int bit = 0; bit < N; ++bit) {
-        if ((maskI >> bit) & 1) {
-          if ((k >> bit) & 1) {
-            idx_res |= (1 << current_pos);
+        if ((maskI >> bit) & 1) { // Si el bit actual es de interés
+          if ((k >> bit) & 1) { 
+            idx_res |= (1 << current_pos); // Indice para el nuevo vector de p
           }
           current_pos++;
         }
       }
-
+      // Acumulamos la probabilidad
       result[idx_res] += p[k];
       prob_evidencia += p[k];
     }
   }
-
+  // Normalizamos el resultado 
   if (prob_evidencia > 0) {
     for (double& val : result) {
       val /= prob_evidencia;
